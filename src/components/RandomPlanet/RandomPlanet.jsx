643020ab7a24ba9b98c3511c63c4c02ed0386f7d
DenysenkoStas <49059676+denysenkostas@users.noreply.github.com>
2021-05-26 17:07:45 +0300

    RandomPlanet.jsx

--- src/components/RandomPlanet/RandomPlanet.jsx
+++ src/components/RandomPlanet/RandomPlanet.jsx
@@ -1,26 +1,59 @@
 import React, {Component} from 'react';
+import SwapiService from '../../services/SwapiService';
 import './RandomPlanet.scss';
 
 export default class RandomPlanet extends Component {
+    swapiService = new SwapiService();
+
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotationPeriod: null,
+        diameter: null
+    };
+
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+
+    updatePlanet() {
+        const id = Math.floor(Math.random() * 25 + 2); // random id
+        this.swapiService
+            .getPlanet(id)
+            .then(planet => {
+                this.setState(({
+                    id,
+                    name: planet.name,
+                    population: planet.population,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter
+                }));
+            });
+    }
+
     render() {
+        const {id, name, population, rotationPeriod, diameter} = this.state;
+
         return (
             <div className='random-planet card p-3'>
                 <img className='random-planet__img card-img'
-                     src='https://starwars-visualguide.com/assets/img/planets/5.jpg' alt='planet'/>
+                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt='planet'/>
                 <div className='card-body'>
-                    <h3>Planet Name</h3>
+                    <h3>{name}</h3>
                     <ul className='list-group list-group-flush'>
                         <li className='list-group-item'>
                             <span className='pe-1'>Population:</span>
-                            <span>123124</span>
+                            <span>{population}</span>
                         </li>
                         <li className='list-group-item'>
                             <span className='pe-1'>Rotation Period:</span>
-                            <span>43</span>
+                            <span>{rotationPeriod}</span>
                         </li>
                         <li className='list-group-item'>
                             <span className='pe-1'>Diameter:</span>
-                            <span>100</span>
+                            <span>{diameter}</span>
                         </li>
                     </ul>
                 </div>
